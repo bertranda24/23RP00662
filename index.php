9d53e666b9ede59aa38256ce73f12e0cfaae7a54
bertrand <bertrairadu@gmail.com>
2024-03-18 15:35:46 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DevOps cat</title>
+</head>
+<body>
+    <?php
+    echo"Welcome to Students MIS";\<br><a href="registration.php">USER REGISTRATION</a></br>
+
+    ?>
+</body>
+</html>
